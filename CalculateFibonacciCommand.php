b65b77931b27310466bc34b31137a71194cf473d
Marijan Šuflaj <msufflaj32@gmail.com>
2019-10-05 17:05:42 +0200

    Version 2

--- CalculateFibonacciCommand.php
+++ CalculateFibonacciCommand.php
@@ -6,16 +6,27 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Lock\Factory;
 
 class CalculateFibonacciCommand extends Command
 {
+
+    /**
+     * @var Factory
+     */
+    private $lockFactory;
+
+    public function __construct(Factory $lockFactory, string $name) {
+        parent::__construct($name);
+        $this->lockFactory = $lockFactory;
+    }
 
     protected function configure()
     {
         $this->addArgument('n', InputArgument::REQUIRED, 'Fibonacci number to calculate');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    private function executeExclusive(InputInterface $input, OutputInterface $output)
     {
         $n = $input->getArgument('n');
 
@@ -28,6 +39,21 @@ class CalculateFibonacciCommand extends Command
         $output->writeln((string) $this->fibonacci((int) $n));
 
         return 0;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $lock = $this->lockFactory->createLock(sha1($this->getName()));
+
+        if (!$lock->acquire()) {
+            return 1;
+        }
+
+        try {
+            return $this->executeExclusive($input, $output);
+        } finally {
+            $lock->release();
+        }
     }
 
     private function fibonacci(int $n): int
